7663c711b841f4db926ddd1b1953a8a97ec39bc3
LucasGuzylak <lucasguzy11@gmail.com>
2025-06-06 15:35:42 -0700

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Lucas Guzylak on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
