eb7c4ac5e117382deeffc91ccf3a65b48e5f970c
LucasGuzylak <lucasguzy11@gmail.com>
2025-07-01 16:44:47 -0700

    Working list

---
+++ FitnessApp/Exercise.swift
@@ -0,0 +1,8 @@
+//
+//  Exercise.swift
+//  FitnessApp
+//
+//  Created by Lucas Guzylak on 6/29/25.
+//
+
+import Foundation
---
+++ FitnessApp/ExerciseViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseViewModel.swift
+//  FitnessApp
+//
+//  Created by Lucas Guzylak on 6/29/25.
+//
+
+import Foundation
